80cbe56bedb09b9affedbdeb53dce4f7caa0858c
temp-flush <646128782@qq.com>
2018-10-31 18:29:02 +0800

    公共部分完成，登录功能完成，用户页面静态完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,68 +1,35 @@
-$(function () {
-    $('form').bootstrapValidator({
-        fields: {
-            username: {
-                validators: {
-                    notEmpty: {
-                        message: '用户名不能为空'
-                    },
-                    stringLength: {
-                        min: 2,
-                        max: 6,
-                        message: '用户名必须为2-6位'
-                    },
-                    callback:{
-                        message: '用户名不存在'
-                    }
-                }
-            },
-            password: {
-                validators: {
-                    notEmpty: {
-                        message: '密码不能为空'
-                    },
-                    stringLength: {
-                        min: 6,
-                        max: 12,
-                        message: '密码必须为6-12位'
-                    },
-                    callback:{
-                        message: '密码错误'
-                    }
-                }
-            }
-        },
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        }
-    });
+$(document).ajaxStart(function () {
+  NProgress.start();
+});
 
-    $("#form").on('success.form.bv', function (e) {
-        e.preventDefault();
-        var validator = $("#form").data('bootstrapValidator');
-        $.ajax({
-            url:'/employee/employeeLogin',
-            data:$('#form').serialize(),
-            dataType:'json',
-            type:'post',
-            success:function(info){
-                console.log(info);
-                if(info.success){
-                    location.href='index.html'
-                }
-                if(info.error==1000) {
-                    validator.updateStatus('username','INVALID', 'callback')
-                }
-                if(info.error==1001) {
-                    validator.updateStatus('password','INVALID', 'callback')
-                }
-            }
-        })
-    });
-    $('[type="reset"]').click(function(){
-        var validator = $("#form").data('bootstrapValidator');
-        validator.resetForm();
-    })
+$(document).ajaxStop(function () {
+  setTimeout(function () {
+    NProgress.done();
+  }, 1000);
+});
+
+
+// 头部公共部分-两个按钮
+
+$('.arror_le').click(function () {
+  $('.lt_aside').toggleClass('active');
+  $('.lt_main').toggleClass('active');
+  $('.lt_main .lt_top').toggleClass('active');
+})
+
+$('.modal #logout').click(function () {
+  $.ajax({
+    type: 'get',
+    url: '/employee/employeeLogout',
+    dataType: 'json',
+    success: function (info) {
+      if (info.success) {
+        location.href = 'login.html';
+      }
+    }
+  })
+})
+
+$('.category').click(function(){
+  $(this).next().slideToggle();
 })
